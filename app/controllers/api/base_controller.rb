ffb3ff570cfb9000372fe6b6d10bde5a03342485
Michael Parreno-Villa <m.parrenovilla@gmail.com>
2011-08-29 20:14:28 +1200

    Define default format for API

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,10 @@
 class Api::BaseController < ApplicationController
   respond_to :json, :xml
   skip_before_filter :prepare_for_mobile
+  before_filter :set_default_format
+  
+  def set_default_format
+    request.format = :xml unless params[:format]
+  end
+  
 end
