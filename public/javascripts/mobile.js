1d8a962d3f45a47ac877ff870fa1e3e7810287a0
Michael Parreno-Villa <m.parrenovilla@gmail.com>
2011-09-05 16:12:56 +1200

    Lots of work on the mobile stuff

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -10,6 +10,7 @@ function scrollToHide(){
 	setTimeout(scrollTo, 0, 0, 1);
 }
 
+//Scripts for the index page
 function findNearestToilets(limit) {
   $('#toilets').html("Loading some crapprs...");
    if (gps_capable()) {
@@ -90,4 +91,69 @@ function getLocation(successCallback, errorCallback) {
     errorCallback();
   }
 }
+
+var map;
+var markers = [];
+
+//window.onresize = trigger_resize;
+
+function trigger_resize() {
+	set_map_height_to($(document).height());
+	google.maps.event.trigger(map, 'resize');
+}
+
+//sets the correct height of the map
+//this should be called onload and onresize
+function set_map_height_to(viewportheight) {
+    var mainHeight = viewportheight - 62;
+    var mapHeight = mainHeight;
+
+    if ($('#map_div')) {
+        $('#map_div').height(mapHeight);
+    }
+		if ($('#pana')) {
+        $('#pana').height(mapHeight);
+    }
+}
+
+function drawMap(latlng, mapType, zoomLevel, id) {
+	  var divid = id || "map_div";
+    var myOptions = {
+        zoom: zoomLevel,
+        center: latlng,
+        mapTypeId: mapType,
+        scrollwheel: false,
+        scaleControl: false,
+        overviewMapControl: false,
+				mapTypeControl: true
+    }
+  
+    map = new google.maps.Map(document.getElementById(divid), myOptions)
+}
+
+//this should be used to face street view guy to a toilet
+function bearing_from(pana,point) 
+{
+	var lat2 = pana.lat(); 
+	var lon2 = pana.lng();
+	var lat1 = point.lat(); 
+	var lon1 = point.lng();
+	
+	var dLat = toRad((lat2-lat1));
+	var dLon = toRad((lon2-lon1));
+	var lat1 = toRad(lat1);
+	var lat2 = toRad(lat2);
+	
+	var y = Math.sin(dLon) * Math.cos(lat2);
+	var x = Math.cos(lat1)*Math.sin(lat2) -
+	        Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+	var brng = (toDeg(Math.atan2(y, x)) + 180) % 360;
+
+	return brng;
+}
+
+function toRad(val){ return val * Math.PI / 180; }
+function toDeg(val){ return val * 180 / Math.PI; }
+
+
 
