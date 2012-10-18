821d6b108036b35a9b623d2dc2ff8cde011dbfce
Josh McArthur <joshua.mcarthur@gmail.com>
2012-10-18 23:39:31 +1300

    Add JSON-only action for listing reviews. Extend create action to respond with JSON

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,22 @@
 class ReviewsController < ApplicationController
   respond_to :js
+
+  def index
+    render :json => toilet.reviews
+  end
+
   def create
-    @review = toilet.reviews.create(params[:review])
+    @review = toilet.reviews.create!(params[:review])
     if @review.valid?
       flash[:notice] = "Thank you for reviewing this toilet"
     else
       flash[:error] = "Unable to add review. Please ensure you have selected a rating and name."
     end
 
-    render
+    respond_to do |format|
+      format.js { render }
+      format.json { render :json => @review }
+    end
   end
 
   def toilet
