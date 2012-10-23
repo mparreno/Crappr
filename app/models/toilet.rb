0d2c4c98d78704ac4afeb5fe608501e2f5b8c1a5
Michael Parreno-Villa <m.parrenovilla@gmail.com>
2012-10-23 16:29:54 +1300

    toilet does not actally need a name

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -7,7 +7,6 @@ class Toilet < ActiveRecord::Base
   belongs_to :suburb
   
   # Validations
-  validates_presence_of :name
   validates_presence_of :location
   validates_numericality_of :lat, :greater_than_or_equal_to => -90.0, :less_than_or_equal_to => 90.0
   validates_numericality_of :lng, :greater_than_or_equal_to => -180.0, :less_than_or_equal_to => 180.0
